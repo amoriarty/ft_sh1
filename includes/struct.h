e1f98f1ed9afc4e34e9ba5737b11af0b831e67c1
alegent <alegent@student.42.fr>
2015-05-19 10:21:59 +0200

    norme

--- includes/struct.h
+++ includes/struct.h
@@ -6,29 +6,29 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/19 10:10:59 by alegent           #+#    #+#             */
-/*   Updated: 2015/05/19 10:11:00 by alegent          ###   ########.fr       */
+/*   Updated: 2015/05/19 10:20:54 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
 
-typedef char						t_bool;
-typedef struct stat					t_stat;
+typedef char							t_bool;
+typedef struct stat						t_stat;
 typedef struct s_lst					t_lst;
-typedef struct s_sh					t_sh;
+typedef struct s_sh						t_sh;
 
-struct							s_lst
+struct									s_lst
 {
-	char						*entry;
-	t_lst						*next;
+	char								*entry;
+	t_lst								*next;
 };
 
-struct							s_sh
+struct									s_sh
 {
-	t_lst						*env;
-	t_lst						*path;
-	char						*home;
+	t_lst								*env;
+	t_lst								*path;
+	char								*home;
 };
 
 #endif
