8d312059edd915ce63d7843afa45c73fb081d386
alegent <alegent@student.42.fr>
2015-05-23 17:22:55 +0200

    handler

--- srcs/ft_handler.c
+++ srcs/ft_handler.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/19 10:13:23 by alegent           #+#    #+#             */
-/*   Updated: 2015/05/21 16:38:51 by alegent          ###   ########.fr       */
+/*   Updated: 2015/05/23 17:22:37 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ static void			ft_signal(int n)
 {
 	if (n == SIGINT)
 	{
-		ft_putchar(EOL);
-		ft_putendl_fd("42sh: SIGINT.", 2);
+		ft_putendl_fd("\n42sh: SIGINT.", 2);
 		ft_putcolor("$> ", GREEN);
 	}
 	if (n == SIGSEGV)
 		ft_sherror("42sh: SIGSEGV.", NULL, TRUE);
 	if (n == SIGTERM)
 		ft_putendl_fd("42sh: SIGTERM.", 2);
-	if (n == SIGHUP)
-		ft_putendl_fd("42sh: SIGHUP.", 2);
+	if (n == SIGQUIT)
+	{
+		ft_putendl_fd("\n42sh: SIGQUIT.", 2);
+		ft_putcolor("$> ", GREEN);
+	}
 }
 
 void				ft_handler(void)
 {
 	signal(SIGINT, ft_signal);
 	signal(SIGTERM, ft_signal);
-	signal(SIGHUP, ft_signal);
 	signal(SIGSEGV, ft_signal);
+	signal(SIGQUIT, ft_signal);
 }
